19dc55e783ba0fbb228c239d346b95f5c6a8c3df
Zoran Petković <peja1971@gmail.com>
2026-01-12 03:43:45 +0100

    WORKING BASE: fullscreen training + layout fixes

--- app/src/main/java/com/peja/eyetrainer/MainActivity.kt
+++ app/src/main/java/com/peja/eyetrainer/MainActivity.kt
@@ -1,47 +1,77 @@
 package com.peja.eyetrainer
 
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.peja.eyetrainer.ui.theme.EyeTrainerTheme
+import android.graphics.Color
+import android.view.View
 
 class MainActivity : ComponentActivity() {
+    private fun hideSystemBarsAlways() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        // BITNO: ne daj da ostanu vidljivi; ako user povuče, vrati se - mi ćemo ih opet sakriti
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            EyeTrainerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+
+        hideSystemBarsAlways()
+
+// PATCH: boja sistema = boja aplikacije (da nema crnih traka)
+        val appBg = Color.parseColor("#e8ecef")
+        window.statusBarColor = appBg
+        window.navigationBarColor = appBg
+
+        val sys = WindowInsetsControllerCompat(window, window.decorView)
+        sys.isAppearanceLightStatusBars = true
+        sys.isAppearanceLightNavigationBars = true
+
+        val webView = WebView(this)
+        webView.setBackgroundColor(appBg)
+        webView.overScrollMode = View.OVER_SCROLL_NEVER
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.settings.cacheMode = WebSettings.LOAD_DEFAULT
+        webView.settings.mediaPlaybackRequiresUserGesture = false
+
+        // ako hoces fullscreen iz JS-a
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+
+        setContentView(webView)
+
+        webView.loadUrl("file:///android_asset/eye_trainer.html")
     }
-}
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemBarsAlways()
+    }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onResume() {
+        super.onResume()
+        hideSystemBarsAlways()
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EyeTrainerTheme {
-        Greeting("Android")
+    override fun onDestroy() {
+        super.onDestroy()
+        // Ako zelis hard cleanup:
+        // (nije obavezno, ali moze)
     }
 }
